151e1b062b4403db013bce46ac943bd9b9c655de
DEOKJAE KWON <juintin@kakao.com>
2024-01-20 02:10:05 +0900

    Add : Simple server, hello-world with Express.js

---
+++ hello-world/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.all('*', (req, res) => {
+	res.render('index', { msg: 'Welcome to the Practical Node.js!' });
+});
+
+http.createServer(app).listen(app.get('port'), () => {
+	console.log('Express server listening on port ' + app.get('port'));
+});
