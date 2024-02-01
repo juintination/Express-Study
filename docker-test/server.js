be0614c992a9afed2f7b201a86f477c769c79f57
DEOKJAE KWON <juintin@kakao.com>
2024-02-01 23:55:27 +0900

    Add : docker-test

---
+++ docker-test/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const PORT = 3000
+
+const app = express()
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+app.listen(PORT, console.log(`Running on http://localhost:${PORT}`))
